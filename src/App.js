9c23cbafd984ea609af1a3e0167bf5bf49273ec5
Roberto Inguanzo <sindhuakula1902@gmail.com>
2021-04-27 21:40:27 -0700

    ll

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Children from './children1'
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1 className="test">Hello this is first app--change</h1>
+      <Children></Children>
+    </div>
+  );
+}
+export default App;
+
---
+++ src/children1.js
@@ -0,0 +1,9 @@
+
+function Children(){
+ return (
+     <div>
+         I am from Children
+         </div>
+ )   
+}
+export default Children;
